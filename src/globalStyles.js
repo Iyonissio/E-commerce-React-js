e37c142ccd3dd0548794d80fc8d47e70ed92314d
Iyonissio <mbryoyo01@gmail.com>
2020-12-16 03:26:00 -0800

    add novos ficheiros

---
+++ src/globalStyles.js
@@ -0,0 +1,10 @@
+import { createGlobalSyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalSyle`
+    * {
+        box-sizing: border-box;
+        margin:0;
+        padding: 0;
+        font-family: 'kanit', sans-serif;
+    }
+`;
