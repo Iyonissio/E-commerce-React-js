18ca47b472c25825bd74592aea3c64a6f28ce45a
Iyonissio <mbryoyo01@gmail.com>
2020-12-17 13:32:50 -0800

    add new files 1 hoje

--- src/components/Hero/HeroElements.js
+++ src/components/Hero/HeroElements.js
@@ -9,3 +9,38 @@ export const HeroContainer = styled.div`
     background-position: center;
     background-size: cover;
 `;
+
+export const HeroContent = styled.div`
+    height: calc(100vh -80px);
+    max-height:100%;
+    width: 100vw;
+    padding:0rem calc((100vw -1300px) /2);
+`;
+
+export const HeroItems = styled.div`
+color: #fff;
+flex-direction: column;
+justify-content: center;
+display: flex;
+align-items: flex-star;
+text-decoration: none;
+cursor: pointer;
+height:100vh
+width: 60px;
+padding:0 2rem;
+text-transform: uppercase;
+line-heigth: 1 ;
+font-weight: bold;
+
+@media screen and (max-width: 659px){
+    width: 100%;
+}
+`;
+
+export const HeroH1 = styled.h1`
+
+`;
+
+export const HeroP = styled.`
+
+`;
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Navbar from '../Navbar';
-import { HeroContainer } from './HeroElements';
+import { HeroContainer, HeroItems, HeroH1,HeroContent,HeroP, HeroBtn } from './HeroElements';
 
 const  Hero = () => {
     return (
         <HeroContainer>
             <Navbar/>
+            <HeroContent>
+                <HeroItems>
+                    <HeroH1>As Melhores Pizzas Sempre</HeroH1>
+                    <HeroP>Pronto em Minutos</HeroP>
+                    <HeroBtn>Efectuar Pedido</HeroBtn>
+                </HeroItems>
+            </HeroContent>
         </HeroContainer>
     );
 };
